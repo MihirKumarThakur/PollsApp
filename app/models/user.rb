3d5595988ede61dd5b483b98c53ab9af3b075688
Ned Ruggeri <ruggeri@self-loop.com>
2014-06-20 00:34:27 -0700

    add User#completed_polls/uncompleted_polls

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,45 @@ class User < ActiveRecord::Base
   )
 
   has_many :responses, foreign_key: :respondent_id
+
+  def completed_polls
+    poll_completion_counts
+      .having("COUNT(DISTINCT questions.id) = COUNT(responses.id)")
+  end
+
+  def uncompleted_polls
+    poll_completion_counts
+      .having("COUNT(DISTINCT questions.id) > COUNT(responses.id)")
+      .having("COUNT(responses.id) > 0")
+  end
+
+  private
+  def poll_completion_counts
+    select_sql = <<-SQL
+polls.*,
+COUNT(DISTINCT questions.id) AS num_questions,
+COUNT(responses.id) AS num_responses
+SQL
+
+    # the interpolation in here is lame (I'm lazy tonight), but it is
+    # actually safe from injection attack because the end-user doesn't
+    # get to pick DB ids.
+    joins_sql = <<-SQL
+LEFT OUTER JOIN (
+  SELECT
+    *
+  FROM
+    responses
+  WHERE
+    responses.respondent_id = #{self.id}
+) AS responses ON answer_choices.id = responses.answer_choice_id
+SQL
+
+    # It annoys me that the `HAVING` can't use the column aliases...
+    Poll
+      .select(select_sql)
+      .joins(questions: :answer_choices)
+      .joins(joins_sql)
+      .group("polls.id")
+  end
 end
