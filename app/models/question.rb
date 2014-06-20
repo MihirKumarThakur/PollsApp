1bfbe77f1d9180ca81852fd1590cdacd2d0af146
Ned Ruggeri <ruggeri@self-loop.com>
2014-06-20 00:25:17 -0700

    simplify Question#results

--- app/models/question.rb
+++ app/models/question.rb
@@ -7,41 +7,29 @@ class Question < ActiveRecord::Base
   has_many :responses, through: :answer_choices
 
   def results
-    # Note that this somewhat tortured solution serves two goals:
-    # 1. Avoid N+1 query
-    # 2. Avoid fetching all the `Response`s (there could be many!)
-
-    select_sql = <<-SQL
-      answer_choices.*,
-      COUNT(responses.id) AS response_count
-    SQL
-
-    responses_joins_sql = <<-SQL
-      LEFT OUTER JOIN
-        responses ON answer_choices.id = responses.answer_choice_id
-    SQL
-
-    answer_choices = self
-      .answer_choices
-      .select(select_sql)
-      .joins(responses_joins_sql)
-      .group("answer_choices.id")
-
-    {}.tap do |results|
-      answer_choices.each do |answer_choice|
-        results[answer_choice.text] =
-          Integer(answer_choice.response_count)
-      end
-    end
-
-    # Here's a simpler solution that fails goal #2:
-    # answer_choices = self
-    #   .answer_choices
-    #   .includes(:responses)
-    # {}.tap do |results|
-    #   answer_choices.each do |ac|
-    #     results[ac.text] = ac.responses.length
-    #   end
+    # N+1 way:
+    # results = {}
+    # self.answer_choices.each do |ac|
+    #   results[ac.text] = ac.responses.count
     # end
+    # results
+
+    # 2-queries; all responses transferred:
+    # results = {}
+    # self.answer_choices.includes(:responses).each do |ac|
+    #   results[ac.text] = ac.responses.length
+    # end
+    # results
+
+    # 1-query way
+    acs = self.answer_choices
+      .select("answer_choices.*, COUNT(responses.id) AS num_responses")
+      .joins(<<-SQL).group("answer_choices.id")
+LEFT OUTER JOIN responses ON answer_choices.id = responses.answer_choice_id
+SQL
+
+    acs.inject({}) do |results, ac|
+      results[ac.text] = ac.num_responses; results
+    end
   end
 end
