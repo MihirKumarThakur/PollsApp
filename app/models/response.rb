2e3c1cd7e3a409f22d9d48cabe5a4704481d3a28
Ryan Glassett <rglassett@gmail.com>
2014-12-31 11:14:15 -0800

    Add one-query solution to sibling_responses

--- app/models/response.rb
+++ app/models/response.rb
@@ -20,6 +20,31 @@ class Response < ActiveRecord::Base
       )
   end
 
+  def sibling_responses_improved
+    # 1-query way
+    binds = { answer_choice_id: self.answer_choice_id, id: self.id }
+    Response.find_by_sql([<<-SQL, binds])
+      SELECT
+        responses.*
+      FROM (
+        SELECT
+          questions.*
+        FROM
+          questions
+        JOIN
+          answer_choices ON questions.id = answer_choices.question_id
+        WHERE
+          answer_choices.id = :answer_choice_id
+      ) AS questions
+      JOIN
+        answer_choices ON questions.id = answer_choices.question_id
+      JOIN
+        responses ON answer_choices.id = responses.answer_choice_id
+      WHERE
+        (:id IS NULL) OR (responses.id != :id)
+    SQL
+  end
+
   private
   def respondent_is_not_poll_author
     # The 3-query slow way:
